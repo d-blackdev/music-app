f1d9e4fb28df2f9c565e98f3f126b643ef902d96
Lasisi <56618398+kenny15335@users.noreply.github.com>
2020-10-15 14:53:08 +0100

    Updates

---
+++ precache-manifest.7571acc93a8f9f72a55b304d7e57312e.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1879e916144bdd535fd28c486fbaef45",
+    "url": "/music-app/index.html"
+  },
+  {
+    "revision": "653dc3f81589b2d0949b",
+    "url": "/music-app/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "9cb4862d99ed0ff85f96",
+    "url": "/music-app/static/css/3.5abcf3fa.chunk.css"
+  },
+  {
+    "revision": "badf44500a3fc77d8e17",
+    "url": "/music-app/static/css/main.6aaaa76c.chunk.css"
+  },
+  {
+    "revision": "653dc3f81589b2d0949b",
+    "url": "/music-app/static/js/2.09ccf250.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/music-app/static/js/2.09ccf250.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cb4862d99ed0ff85f96",
+    "url": "/music-app/static/js/3.16bc0809.chunk.js"
+  },
+  {
+    "revision": "badf44500a3fc77d8e17",
+    "url": "/music-app/static/js/main.9a6225f3.chunk.js"
+  },
+  {
+    "revision": "7a5ab3e02625f2be2f10",
+    "url": "/music-app/static/js/runtime-main.10352ce2.js"
+  },
+  {
+    "revision": "d2dc3b618fde029ec76fc08bea28a04f",
+    "url": "/music-app/static/media/Logo.d2dc3b61.png"
+  },
+  {
+    "revision": "938bd7aeeedb4de52775205ff1c4a5a4",
+    "url": "/music-app/static/media/img.938bd7ae.jpg"
+  },
+  {
+    "revision": "dff61c438f90443fc278830f56ec96ed",
+    "url": "/music-app/static/media/img1.dff61c43.jpg"
+  },
+  {
+    "revision": "be1805800c99ace0329cfed2879c26eb",
+    "url": "/music-app/static/media/img2.be180580.jpg"
+  },
+  {
+    "revision": "c7c9a38db76bfba6d631966f0ef093ba",
+    "url": "/music-app/static/media/img3.c7c9a38d.jpg"
+  },
+  {
+    "revision": "bf9b7a76a6feca0177f78dc1ed38a6f1",
+    "url": "/music-app/static/media/img4.bf9b7a76.jpg"
+  }
+]);
