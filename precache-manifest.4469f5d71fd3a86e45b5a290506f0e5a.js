c1fefcc078cee37c352d2ae9a7747c80cfb3039e
Lasisi Kehinde <56618398+kenny15335@users.noreply.github.com>
2020-10-20 10:25:55 +0100

    Updates

---
+++ precache-manifest.4469f5d71fd3a86e45b5a290506f0e5a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15fb821f554cdeee01645bac0f19cc23",
+    "url": "/music-app/index.html"
+  },
+  {
+    "revision": "0102774d7cfd87246e86",
+    "url": "/music-app/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "451e40984a35781a92f4",
+    "url": "/music-app/static/css/main.d241238f.chunk.css"
+  },
+  {
+    "revision": "0102774d7cfd87246e86",
+    "url": "/music-app/static/js/2.cc56696f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/music-app/static/js/2.cc56696f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "451e40984a35781a92f4",
+    "url": "/music-app/static/js/main.0d8ddb66.chunk.js"
+  },
+  {
+    "revision": "4f04cc2715f387e37000",
+    "url": "/music-app/static/js/runtime-main.726d910a.js"
+  },
+  {
+    "revision": "d2dc3b618fde029ec76fc08bea28a04f",
+    "url": "/music-app/static/media/Logo.d2dc3b61.png"
+  },
+  {
+    "revision": "938bd7aeeedb4de52775205ff1c4a5a4",
+    "url": "/music-app/static/media/img.938bd7ae.jpg"
+  },
+  {
+    "revision": "dff61c438f90443fc278830f56ec96ed",
+    "url": "/music-app/static/media/img1.dff61c43.jpg"
+  },
+  {
+    "revision": "be1805800c99ace0329cfed2879c26eb",
+    "url": "/music-app/static/media/img2.be180580.jpg"
+  },
+  {
+    "revision": "c7c9a38db76bfba6d631966f0ef093ba",
+    "url": "/music-app/static/media/img3.c7c9a38d.jpg"
+  },
+  {
+    "revision": "bf9b7a76a6feca0177f78dc1ed38a6f1",
+    "url": "/music-app/static/media/img4.bf9b7a76.jpg"
+  },
+  {
+    "revision": "7653bd4c151abfb25c1cc48a950f5a21",
+    "url": "/music-app/static/media/img5.7653bd4c.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5168c280a42ce10992b8bddb98603cd2.js"
+  "/music-app/precache-manifest.4469f5d71fd3a86e45b5a290506f0e5a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/music-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
